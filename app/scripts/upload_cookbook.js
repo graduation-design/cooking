a9da0ab17f5f1d68bdb832384d4523c459184a5c
luekeisme <luekeisme2013@gmail.com>
2015-03-24 17:27:09 +0800

    cookbook edit page

---
+++ app/scripts/upload_cookbook.js
@@ -0,0 +1,17 @@
+(function (document) {
+  'use strict';
+
+  // Grab a reference to our auto-binding template
+  // and give it some initial binding values
+  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+  var uploadCookbook = document.querySelector('#upload_cookbook');
+
+  // Listen for template bound event to know when bindings
+  // have resolved and content has been stamped to the page
+  uploadCookbook.addEventListener('template-bound', function() {
+    console.log('Our app is ready to rock!');
+  });
+
+// wrap document so it plays nice with other libraries
+// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+})(wrap(document));
