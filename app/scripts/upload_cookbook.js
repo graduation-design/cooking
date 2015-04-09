f4ce2ea65b04ea8ccef3ca9570daa97c4e2515dd
lukeisme <lukeisme2013@gmail.com>
2015-04-09 17:16:06 +0800

    dev

--- app/scripts/upload_cookbook.js
+++ app/scripts/upload_cookbook.js
@@ -1,17 +1,12 @@
 (function (document) {
   'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var uploadCookbook = document.querySelector('#upload_cookbook');
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
+  uploadCookbook.ingredients = [{food_name: '', food_quantity: ''},{food_name: '', food_quantity: ''}];
+  uploadCookbook.steps = [{step_content:''},{step_content:''},{step_content:''}];
+
   uploadCookbook.addEventListener('template-bound', function() {
     console.log('Our app is ready to rock!');
   });
-
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
--- dist/scripts/upload_cookbook.js
+++ dist/scripts/upload_cookbook.js
@@ -1,1 +1,1 @@
-!function(o){"use strict";var e=o.querySelector("#upload_cookbook");e.addEventListener("template-bound",function(){console.log("Our app is ready to rock!")})}(wrap(document));
+!function(o){"use strict";var t=o.querySelector("#upload_cookbook");t.ingredients=[{food_name:"",food_quantity:""},{food_name:"",food_quantity:""}],t.steps=[{step_content:""},{step_content:""},{step_content:""}],t.addEventListener("template-bound",function(){console.log("Our app is ready to rock!")})}(wrap(document));
