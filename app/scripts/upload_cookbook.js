516bf96f662132f18db35c61312f1de393fcbb2b
lukeisme <lukeisme2013@gmail.com>
2015-04-10 11:43:32 +0800

    dev

--- app/scripts/upload_cookbook.js
+++ app/scripts/upload_cookbook.js
@@ -3,10 +3,12 @@
 
   var uploadCookbook = document.querySelector('#upload_cookbook');
 
-  uploadCookbook.ingredients = [{food_name: '', food_quantity: ''},{food_name: '', food_quantity: ''}];
-  uploadCookbook.steps = [{step_content:''},{step_content:''},{step_content:''}];
+  uploadCookbook.ingredients = [{food_name: '', food_quantity: ''}, {food_name: '', food_quantity: ''}];
+  uploadCookbook.steps = [{step_content: ''}, {step_content: ''}, {step_content: ''}];
 
-  uploadCookbook.addEventListener('template-bound', function() {
-    console.log('Our app is ready to rock!');
+  uploadCookbook.addEventListener('template-bound', function () {
+    document.addEventListener('click', function(){
+      console.log(document.querySelector('file-input').files);
+    });
   });
 })(wrap(document));
